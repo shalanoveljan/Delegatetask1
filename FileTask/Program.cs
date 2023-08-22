ea870f427be4e9746ba18855510c2053aa5bbba3
shalanoveljan <138590101+shalanoveljan@users.noreply.github.com>
2023-08-22 20:52:55 +0400

    Update Program.cs

--- FileTask/Program.cs
+++ FileTask/Program.cs
@@ -1,7 +1,26 @@
 ﻿using FileTask;
 
-Excel e=null;
-Word w=null;
-Pdf p=null;
+string choice = Console.ReadLine().Trim().ToLower();
 
-MainPrintFile MPF = new MainPrintFile(e,w,p);
+            object instance;
+
+            switch (choice)
+            {
+                case "word":
+                    instance = new MainPrintFile(new Word());
+                    break;
+                case "excel":
+                    instance = new MainPrintFile(new Excel());
+                    break;
+                case "pdf":
+                    instance = new MainPrintFile(new PDF());
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice! Please enter Word, Excel, PDF.");
+                    continue;
+            }
+
+            ((MainPrintFile)instance).Print();
+        }
+    }
+}
